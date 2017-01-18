e1093574abf19afeae8794b449d1172ec235fe0c
shenghou90 <shenghou90@gmail.com>
2017-01-18 17:19:27 +0800

    Update SocialFeeder.php

--- src/Despark/LaravelSocialFeeder/SocialFeeder.php
+++ src/Despark/LaravelSocialFeeder/SocialFeeder.php
@@ -1,69 +1,122 @@
 <?php namespace Despark\LaravelSocialFeeder;
-
+use Illuminate\Support\Facades\Log;
 use Config;
-use SammyK\LaravelFacebookSdk\LaravelFacebookSdk;
+
 use Facebook;
+
+//require_once  base_path() . '/vendor/facebook/graph-sdk/src/Facebook/autoload.php';
 
 class SocialFeeder {
 
-	public static function updateTwitterPosts()
-	{
+    public static function updateTwitterPosts()
+    {
         $connection = new \Abraham\TwitterOAuth\TwitterOAuth(
             Config::get('laravel-social-feeder::twitterCredentials.consumerKey'),
             Config::get('laravel-social-feeder::twitterCredentials.consumerSecret'),
             Config::get('laravel-social-feeder::twitterCredentials.accessToken'),
             Config::get('laravel-social-feeder::twitterCredentials.accessTokenSecret')
         );
+        Log::debug(Config::get('laravel-social-feeder::twitterCredentials.consumerKey'));
+        Log::debug( Config::get('laravel-social-feeder::twitterCredentials.consumerSecret'));
+        Log::debug( Config::get('laravel-social-feeder::twitterCredentials.accessToken'));
+        Log::debug( Config::get('laravel-social-feeder::twitterCredentials.accessTokenSecret'));
 
-		$params = array(
+        $params = array(
             'screen_name' => Config::get('laravel-social-feeder::twitterCredentials.screen_name'),
-            'count' => 10,
+            'count' => Config::get('laravel-social-feeder::twitterCredentials.limit'),
         );
 
-		$lastTwitterPost = \SocialPost::type('twitter')
-    		->latest('published_at')
-    		->limit('1')
-    		->get()
-    		->first();
+        $lastTwitterPost = \SocialPost::type('twitter')
+            ->latest('published_at')
+            ->limit('1')
+            ->get()
+            ->first();
 
-    	if ($lastTwitterPost)
-    	{
-    		$params['since_id'] = $lastTwitterPost->social_id;
-    	}
-
+        if ($lastTwitterPost)
+        {
+            $params['since_id'] = $lastTwitterPost->social_id;
+        }
         try
         {
             $tweets = $connection->get('statuses/user_timeline', $params);
         }
         catch (Exception $e)
-       	{
+        {
             $tweets = array();
         }
-
+        //Log::debug("here SH: " . print_r($tweets, true));
+        $outputs = array();
         foreach ($tweets as $tweet)
         {
-        	if ( ! is_object($tweet))
+            if ( ! is_object($tweet))
                 continue;
 
-			$newPostData = [
-	            'type' => 'twitter',
-				'social_id' => $tweet->id_str,
-				'url' => 'https://twitter.com/'.$params['screen_name'].'/status/'.$tweet->id_str,
-				'text' => $tweet->text,
-				'show_on_page' => 1,
-				'published_at' => date('Y-m-d H:i:s', strtotime($tweet->created_at)),
+            $newPostData = [
+                'type' => 'twitter',
+                'social_id' => $tweet->id_str,
+                'url' => 'https://twitter.com/'.$params['screen_name'].'/status/'.$tweet->id_str,
+                'text' => $tweet->text,
+                'show_on_page' => 1,
+                'author_name' => $tweet->user->name,
+                'author_image_url' => $tweet->user->profile_image_url,
+                'published_at' => date('Y-m-d H:i:s', strtotime($tweet->created_at)),
             ];
 
-	        $newPostEntity = new \SocialPost;
-	        $newPostEntity->fill($newPostData)->save();
+            //$newPostEntity = new \SocialPost;
+            //$newPostEntity->fill($newPostData)-save();
+            array_push($outputs, $newPostData);
         }
-
-        return true;
-	}
+        return $outputs;
+    }
 
     public static function updateFacebookPosts()
     {
-        Facebook::setAccessToken(Config::get('laravel-social-feeder::facebookCredentials.accessToken'));
+        $pageId = Config::get('laravel-social-feeder::facebookCredentials.pageName');
+        $limit = Config::get('laravel-social-feeder::facebookCredentials.limit');
+
+        // Get the name of the logged in user
+        $appId = Config::get('laravel-social-feeder::facebookCredentials.appId');
+        $appSecret = Config::get('laravel-social-feeder::facebookCredentials.appSecret');
+
+        $url = 'https://graph.facebook.com/' . $pageId . '/feed?fields=full_picture,from{name,picture},message,created_time,id,permalink_url&limit=' . $limit . '&access_token=' . $appId . '|' . $appSecret;
+
+        // Initializing curl
+        $ch = curl_init( $url );
+
+        // Configuring curl options
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => array('Content-type: application/json')
+        );
+
+        // Setting curl options
+        curl_setopt_array( $ch, $options );
+
+        // Getting results
+        $results = curl_exec($ch); // Getting jSON result string
+        $results = json_decode($results);
+        $results = $results->data;
+        $outputs = array();
+        foreach ($results as $post) {
+            Log::debug("here SHH1: " . print_r($post, true));
+            $message = empty($post->message) ? "" : $post->message;
+            $imageUrl = empty($post->full_picture) ? "" : $post->full_picture;
+            $newPostData = array(
+                'type' => 'facebook',
+                'social_id' => $post->id,
+                'url' => $post->permalink_url,
+                'text' => $message,
+                'image_url' => $imageUrl,
+                'show_on_page' => 1,
+                'published_at' => date('Y-m-d H:i:s', strtotime($post->created_time)),
+            );
+            array_push($outputs, $newPostData);
+        }
+
+
+        /*commented originally from despark
+         * Facebook::setAccessToken(Config::get('laravel-social-feeder::facebookCredentials.accessToken'));
 
         $pageName = Config::get('laravel-social-feeder::facebookCredentials.pageName');
 
@@ -95,9 +148,9 @@ class SocialFeeder {
 
             $newPostEntity = new \SocialPost;
             $newPostEntity->fill($newPostData)->save();
-        }
+        }*/
 
-        return true;
+        return $outputs;
     }
 
     public static function updateInstagramPosts()
@@ -105,35 +158,43 @@ class SocialFeeder {
         $lastInstagramPost = \SocialPost::type('instagram')->latest('published_at')->get()->first();
         $lastInstagramPostTimestamp = $lastInstagramPost ? strtotime($lastInstagramPost->published_at) : 0;
 
-        $clientId = Config::get('laravel-social-feeder::instagramCredentials.clientId');
+        //$clientId = Config::get('laravel-social-feeder::instagramCredentials.clientId');
         $userId = Config::get('laravel-social-feeder::instagramCredentials.userId');
+        $accessToken = Config::get('laravel-social-feeder::instagramCredentials.accessToken');
+        $limit = Config::get('laravel-social-feeder::instagramCredentials.limit');
 
-        $url = 'https://api.instagram.com/v1/users/'.$userId.'/media/recent?count=5&client_id='.$clientId;
+        $url = 'https://api.instagram.com/v1/users/'.$userId.'/media/recent?access_token=' . $accessToken . "&count=" . $limit;;
         $json = file_get_contents($url);
 
         $obj = json_decode($json);
 
         $postsData = $obj->data;
-
+        Log::debug('$postsData: ' . print_r($postsData, true));
+        $outputs = array();
         foreach ($postsData as $post)
         {
-            if ( $post->caption->created_time <= $lastInstagramPostTimestamp)
-                continue;
+            if(!is_null($post->caption)) {
+                if ($post->caption->created_time <= $lastInstagramPostTimestamp)
+                    continue;
 
-            $newPostData = array(
-                'type' => 'instagram',
-                'social_id' => $post->caption->id,
-                'url' => $post->link,
-                'text' => $post->caption->text,
-                'image_url' => $post->images->standard_resolution->url,
-                'show_on_page' => 1,
-                'published_at' => date('Y-m-d H:i:s', $post->caption->created_time),
-            );
+                $newPostData = array(
+                    'type' => 'instagram',
+                    'social_id' => $post->id,
+                    'url' => $post->link,
+                    'text' => $post->caption->text,
+                    'image_url' => $post->images->standard_resolution->url,
+                    'show_on_page' => 1,
+                    'author_name' => $post->user->username,
+                    'author_image_url' => $post->user->profile_picture,
+                    'published_at' => date('Y-m-d H:i:s', $post->caption->created_time),
+                );
 
-            $newPostEntity = new \SocialPost;
-            $newPostEntity->fill($newPostData)->save();
+               // $newPostEntity = new \SocialPost;
+                //$newPostEntity->fill($newPostData)->save();
+                array_push($outputs, $newPostData);
+            }
         }
 
-        return true;
+        return $outputs;
     }
 }
